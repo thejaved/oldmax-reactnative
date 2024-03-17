c76ad3779520c2117ba54d907a657da7bd20bbdb
Javed Khan <javedkhan@Javeds-MacBook-Air.local>
2024-03-17 19:57:43 +0530

    Name changed

--- ios/ImageGenTests/ImageGenTests.m
+++ ios/ImageGenTests/ImageGenTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ImageGenTests : XCTestCase
+@interface OldmaxTests : XCTestCase
 
 @end
 
-@implementation ImageGenTests
+@implementation OldmaxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
